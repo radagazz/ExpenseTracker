70c05dac5f4bc4458625f285c7d680a97080837e
Anurak Teerarattananukulchai <anurak.teera@gmail.com>
2020-09-02 01:59:03 +0700

    Completed

---
+++ ExpenseTracker/AddView.swift
@@ -0,0 +1,21 @@
+//
+//  AddView.swift
+//  ExpenseTracker
+//
+//  Created by anurak teerarattananukulchai on 2/9/2563 BE.
+//  Copyright © 2563 anurak teerarattananukulchai. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView()
+    }
+}
