07b793af07235401fbdb8fed78054e7f14e39ed8
Anurak Teerarattananukulchai <anurak.teera@gmail.com>
2020-09-02 01:11:51 +0700

    Initial Commit

---
+++ ExpenseTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExpenseTracker
+//
+//  Created by anurak teerarattananukulchai on 2/9/2563 BE.
+//  Copyright © 2563 anurak teerarattananukulchai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
